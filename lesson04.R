1cd7d4d7761a42bbab9a02d33c5d58229f67bbe6
Onur DÖKER <odoker_sup@outlook.com>
2026-04-09 15:25:36 +0300

    Lesson04 uploaded

--- lesson04.R
+++ lesson04.R
@@ -159,3 +159,198 @@ Tidyverse is a collection of packages developed for data science
 
 # installing tidyverse
 install.packages('tidyverse')
+
+# calling tidyverse
+library(tidyverse)
+
+
+install.packages('nycflights13')
+library(nycflights13)
+
+View(flights)
+
+''
+'
+DATA TRANSFORMATION
+
+For data transformation, we use five main functions available in the dplyr package:
+  - filter () : Selecting observations based on values
+  - arrange () : Reordering rows
+  - select () : Selecting variables by name
+  - mutte () : Deriving new variables from existing variables
+  - summarize () : Converting multiple values into a summary value
+'
+''
+
+# Filtering flights that occurred on January 1st
+filter(
+  flights,
+  month == 1,
+  day == 1
+) # 842 flights
+
+# Filtering flights that occurred on December 25th
+filter(
+  flights,
+  month == 12,
+  day == 25
+) # 719 flights
+
+# Filtering all flights that occurred in November and December
+filter(
+  flights,
+  month == 11 | month == 12
+) # 55403 flights
+
+filter(
+  flights,
+  month %in% c(11, 12)
+) # 55403 flights
+
+# Filtering all flights with a delay of under two hours
+
+filter(
+  flights,
+  !(dep_delay > 120) | !(arr_delay) > 120
+) # 320060 flights
+
+filter(
+  flights,
+  dep_delay <= 120 | arr_delay <= 120
+) # 320060 flights
+
+# Filtering all flights with an arrival delay exceeding two hours
+filter(
+  flights,
+  arr_delay >= 120
+) # 10200 flights
+
+# Filtering all flights to Houston (AIH or HOU)
+filter(
+  flights,
+  dest == 'AIH' | dest == 'HOU'
+) # 2115 flights
+
+filter(
+  flights,
+  dest %in% c('AIH', 'HOU')
+) # 2115 flights
+
+# Filtering all flights operated by United, Delta and American Airlines
+airlines[grepl('United|American|Delta', airlines$name), ]
+
+filter(
+  airlines,
+  grepl('United|American|Delta', name)
+)
+
+filter(
+  flights,
+  carrier %in% c('AA', 'DL', 'AU')
+) # 80839 flights
+
+# Filtering all flights deparing in July, August and September
+filter(
+  flights,
+  month >= 7,
+  month <= 9
+) # 86326 flights
+
+filter(
+  flights,
+  month %in% c(7:9)
+) # 86326 flights
+
+filter(
+  flights,
+  month %in% c(7, 8, 9)
+) # 86326 flights
+
+# Filtering all flights de departure delay and an arrival delay greater than two hours
+filter(
+  flights,
+  dep_delay == 0 & arr_delay > 120
+) # 3 flights
+
+# How many flights have an NA value for departure delay
+filter(
+  flights,
+  is.na(dep_delay)
+)
+
+# Sort the flight information
+arrange(
+  flights,
+  year,
+  month,
+  day
+)
+
+# Sorting the flights by departure delay
+arrange(
+  flights,
+  desc(dep_delay)
+)
+
+# Sorting the flights from fastest to the slowest
+arrange(
+  flights,
+  desc(
+    distance / air_time
+  )
+)
+
+# Create a new dataset that contains only the data we want, from the original dataset
+select(
+  flights,
+  year,
+  month,
+  day
+)
+
+select(
+  flights,
+  year:day
+)
+
+# Appending new data to the end of the dataset
+#
+flights_sml <- select(
+  flights,
+  year:day,
+  dep_delay,
+  arr_delay,
+  distance,
+  air_time
+)
+mutate(
+  flights_sml,
+  gain = dep_delay - arr_delay,
+  speed = distance / air_time * 60
+)
+
+# Summarising the flights by arrival delay
+summarise(
+  flights,
+  delay = mean(
+    dep_delay,
+    na.rm = TRUE
+  )
+) # 12.6
+
+# Summarizing the flights for each day, based on arrival delay
+by_day <- group_by(
+  flights,
+  year,
+  month,
+  day
+)
+summarise(
+  by_day,
+  delay = mean(
+    dep_delay,
+    na.rm = TRUE
+  )
+) # summarise with 365 lines
+
+save.image(file = "lesson04.RData")
